6cbb287cb37e06285160da5ce0209aaa6baf3d4f
Rurik02 <manu.dag20@gmail.com>
2022-06-23 22:30:32 +0200

    aggiustamenti oneline

--- Python/Esercizi/OneLine.py
+++ Python/Esercizi/OneLine.py
@@ -71,13 +71,12 @@ def es4(a, b):
 
 
 
-
 #%%
 """ ES 5 - medio/facile
 Scrivere una funzione che prenda come parametro 2 liste, faccia la moltiplicazione tra gli elementi dello stesso indice e ritorni una 
 lista contenente i valori moltiplicati solo se multipli di 5.
 """
-def es (lista1, lista2):
+def es5(lista1, lista2):
     #Inserisci qui il tuo codice
     pass
 
@@ -88,19 +87,28 @@ def es (lista1, lista2):
 Scrivere una funzione che, date due liste di interi, ritorni una lista contenente i quadrati dei valori assoluti maggiori di 10 della 
 divisione dei multipli di 3.
 """
-def es (lista1, lista2):
+def es6(lista1, lista2):
     #Inserisci qui il tuo codice
     pass
 
 
 
 #%%
-"""es 7 - difficile
-Progettare una che prenda in inuput due stringhe di soli numeri (es: a = "3542189432185976" e b = "4568281346714923") consdierati numeri 
+""" ES 7 - difficile
+Progettare una che prenda in inuput due stringhe di soli numeri (es: a = "3542189432185976" e b = "4568281346714923") considerati numeri 
 a una cifra, li converta in interi e per ogni numero della prima stringa ne faccia il prodotto con ogni numero della seconda, salvando
 ogni valore ottenuto in una nuova lista (che verrà poi ritornata).
 """
-def es (a, b):
+def es7(a, b):
     #Inserisci qui il tuo codice
     pass
 
+
+
+#%%
+""" ES 8 - 
+
+"""
+def es8(a, b):
+    #Inserisci qui il tuo codice
+    pass
