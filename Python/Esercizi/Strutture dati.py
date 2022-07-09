6394c1236f427e5f7da98ef8f53b8555c5b6175e
Rurik02 <manu.dag20@gmail.com>
2022-07-09 21:00:22 +0200

    strutture dati 1

--- Python/Esercizi/Strutture dati.py
+++ Python/Esercizi/Strutture dati.py
@@ -1,5 +1,57 @@
 """
 Autor: Rurik
 
+In questo file verranno trattati esercizi inerenti a liste, dizionari, tuple e insiemi.
+Non sono necessarie conoscenze di altri argomenti per affrontare gli esercizi.
+"""
+
+
+
+#%%
+"""
+ES 0 - banale
 
 """
+
+def es0():
+    # inserisci qui il tuo codice
+    pass
+
+
+
+
+#%%
+"""
+ES 1 - 
+
+"""
+
+def es1():
+    # inserisci qui il tuo codice
+    pass
+
+
+
+
+#%%
+"""
+ES 2 - 
+
+"""
+
+def es2():
+    # inserisci qui il tuo codice
+    pass
+
+
+
+
+#%%
+"""
+ES 3 - 
+
+"""
+
+def es3():
+    # inserisci qui il tuo codice
+    pass
