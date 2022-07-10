acba33177b97bab2f804dd9541702933b35f841b
Rurik02 <manu.dag20@gmail.com>
2022-07-10 13:45:49 +0200

    es 0,1 strutture dati

--- Python/Esercizi/Strutture dati.py
+++ Python/Esercizi/Strutture dati.py
@@ -10,25 +10,46 @@ Non sono necessarie conoscenze di altri argomenti per affrontare gli esercizi.
 #%%
 """
 ES 0 - banale
-
+Data in input una lista di valori, restituire un dizionario contenente per chiavi i valori contenuti nella lista e per valori il numero di
+occorrenze del valore nella lista
 """
 
-def es0():
+def es0(lista):
     # inserisci qui il tuo codice
     pass
+
+# print(es0([1, 2, 4, 2, 6, 3, 4, 2, 2, 1, 0, 9]))
+
+# OUT:  {1: 2, 
+#        2: 4, 
+#        4: 2, 
+#        6: 1, 
+#        3: 1, 
+#        0: 1, 
+#        9: 1}
 
 
 
 
 #%%
 """
-ES 1 - 
+ES 1 - banale
+Data in input una stringa, ritornare una stringa modificata secondo i seguenti parametri:
+    'a' -> '4'      'A' -> 'a'      'm' -> 'N'      'x', 'y', 'z' -> '*'
+    'e' -> '3'      'E' -> 'e'      'M' -> 'n'      'k', 'j', 'w' -> '#'
+    'i' -> '1'      'I' -> 'i'      'n' -> 'M'      
+    'o' -> '0'      'O' -> 'o'      'N' -> 'm'      
 
+Infine gli spazi ' ' vengono tradotti in trattini bassi '_'.
 """
 
-def es1():
+def es1(stringa):
     # inserisci qui il tuo codice
     pass
+
+# print(es1("Testo di Esempio cOn Molta fantasia xyz kjw"))
+
+# OUT : T3st0_d1_es3Np10_coM_n0lt4_f4Mt4s14_***_###
 
 
 
