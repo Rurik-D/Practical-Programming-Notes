d7569f0ffa68f0bb38c2951623d0ad5160566cd0
Rurik02 <manu.dag20@gmail.com>
2022-08-19 16:15:28 +0200

    Update 0_introduzione.asm

--- Assembly (mips)/Appunti/0_introduzione.asm
+++ Assembly (mips)/Appunti/0_introduzione.asm
@@ -9,21 +9,21 @@
                     #######
 			
 # La Central Processing Unit è formata da:
-# 	• Unità di elaborazione Aritmetico/Logica (ALU)
-#		.Fa solo calcoli
-#		.Non ha uno stato interno
+#   • Unità di elaborazione Aritmetico/Logica (ALU)
+#       .Fa solo calcoli
+#       .Non ha uno stato interno
 
-#	• La Control Unit
-#		.Coordinare tutte le azioni necessarie per 
-#		 l'esecuzione di una istruzione o insiemi di istr.
+#   • La Control Unit
+#       .Coordinare tutte le azioni necessarie per 
+#        l'esecuzione di una istruzione o insiemi di istr.
 
-#	• I Registri
-#		.A uso generale e speciale
-#		.Consentono manipolazione di istruzioni, dati,
-#		 indirizzi, risultati
+#   • I Registri
+#       .A uso generale e speciale
+#       .Consentono manipolazione di istruzioni, dati,
+#        indirizzi, risultati
 
-#	• Il Bus di comunicazione con la Memoria
-#	• Il Bus di comunicazione con le Perifieriche (in/out)
+#   • Il Bus di comunicazione con la Memoria
+#   • Il Bus di comunicazione con le Perifieriche (in/out)
 
 
                 #################
@@ -38,29 +38,29 @@
 # Ciascuna word conteneva 2 istruzioni.
 
 # Componenti della sua CPU:
-#	• MBR (Memory Buffer Register)
-#		.Riceve/manda dati dalla/alla memoria
+#   • MBR (Memory Buffer Register)
+#       .Riceve/manda dati dalla/alla memoria
 
-#	• MAR (Memory Address Register)
-#		.Indica l'indirizzo della locazione di memoria RAM
-#		 in cui si andrà a leggere/scrivere un dato
+#   • MAR (Memory Address Register)
+#       .Indica l'indirizzo della locazione di memoria RAM
+#        in cui si andrà a leggere/scrivere un dato
 
-#	• PC  (Program Counter)
-#		.Registro contente l'indirizzo dell'istruzione 
-#		 in esecuzione
+#   • PC  (Program Counter)
+#       .Registro contente l'indirizzo dell'istruzione 
+#        in esecuzione
 
-#	• IR  (Instruction Register)
-#		.Riceve l'istruzione da eseguire
+#   • IR  (Instruction Register)
+#       .Riceve l'istruzione da eseguire
 
-#	• IBR (Instruction Buffer Register)
-#		.Memorizza temporaneamente la seconda istruzione
-#		 della word
+#   • IBR (Instruction Buffer Register)
+#       .Memorizza temporaneamente la seconda istruzione
+#        della word
 
-#	• AC  (Accumulatore)
-#		.Per i risultati parziali dei caloli della ALU
+#   • AC  (Accumulatore)
+#       .Per i risultati parziali dei caloli della ALU
 
-#	• MQ  (Multiplier Quotient)
-#		.Per i risultati parziali dei caloli della ALU
+#   • MQ  (Multiplier Quotient)
+#       .Per i risultati parziali dei caloli della ALU
 
 
                     ########
@@ -68,19 +68,19 @@
                     ########
 
 # Il MIPS è un'architettura di tipo RISC: (Reduced Instruction Set Computer)
-#    • Istruzioni di dimensione fissa
-#        .Fetch(vedi dopo) della successiva senza decodifica della prec.
-#    • Istruzioni di formato uniforme
-#        .Per semplificare la fase di decodifica
-#    • Operazioni ALU solo tra registri
-#        .Senza accesso a memoria
-#    • Molti registri interni
-#        .Per i risultati parziali senza accessi alla memoria
-#    • Modi di indirizzamento semplici
-#        .Con spiazzamento, 1 solo accesso a memoria
-#        .Durata fissa della istruzione
-#        .Conflitti semplici
-#    • Istruz. semplici => pipeline più veloce
+#   • Istruzioni di dimensione fissa
+#       .Fetch(vedi dopo) della successiva senza decodifica della prec.
+#   • Istruzioni di formato uniforme
+#       .Per semplificare la fase di decodifica
+#   • Operazioni ALU solo tra registri
+#       .Senza accesso a memoria
+#   • Molti registri interni
+#       .Per i risultati parziali senza accessi alla memoria
+#   • Modi di indirizzamento semplici
+#       .Con spiazzamento, 1 solo accesso a memoria
+#       .Durata fissa della istruzione
+#       .Conflitti semplici
+#   • Istruz. semplici => pipeline più veloce
 
 # Le istruzioni sono tutte a 32 bit.
 # BYTE(8 bits), HALFWORD (2 bytes), WORD (4 bytes).
@@ -98,8 +98,8 @@
 
 # Abbiamo 32 registri divisi in gruppi, ogni registro è preceduto da $.
 # 2 formati per usare i registri:
-#	• Usando il numero del registro, da $0 a $31
-#	• Usando i nomi equivalenti, $t1, $sp ecc..
+#   • Usando il numero del registro, da $0 a $31
+#   • Usando i nomi equivalenti, $t1, $sp ecc..
 
 # I registri speciali "lo" e "hi", vengono utilizzati dall'assembler per i
 # risultati parziali di moltiplicazioni e divisioni, direttamente indirizzabili
